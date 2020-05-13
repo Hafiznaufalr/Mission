b36e60fe9b58da8c8bff5537793476092bde1e2f
hafiz <hafiz.nr5758@gmail.com>
2020-05-13 13:55:59 +0700

    Challenge: Add an Animation

--- app/src/main/java/com/raywenderlich/timefighter/hafiznaufalr/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/hafiznaufalr/MainActivity.kt
@@ -3,6 +3,7 @@ package com.raywenderlich.timefighter.hafiznaufalr
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -38,6 +39,8 @@ class MainActivity : AppCompatActivity() {
         timeLeftTextView = findViewById(R.id.timeLifeTextView)
 
         tapMeButton.setOnClickListener { view ->
+            val bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce)
+            view.startAnimation(bounceAnimation)
             incrementScore()
         }
 
@@ -95,6 +98,9 @@ class MainActivity : AppCompatActivity() {
         score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreTextView.text = newScore
+
+        val blinkAnimation = AnimationUtils.loadAnimation(this, R.anim.blink)
+        gameScoreTextView.startAnimation(blinkAnimation)
     }
 
     private fun resetGame() {
